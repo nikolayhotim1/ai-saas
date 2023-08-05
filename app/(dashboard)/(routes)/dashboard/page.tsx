d59b1aadd1818c42e2df70ddd51edb4d0ec8f82c
nikolayhotim1 <hotim_nikolay@mail.ru>
2023-08-05 16:19:56 +0300

    feature: folder setup, clerk authentication

---
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -0,0 +1,10 @@
+import { UserButton } from '@clerk/nextjs'
+
+export default function DashboardPage() {
+	return (
+		<div>
+			Dashboard Page (Protected)
+			<UserButton afterSignOutUrl='/' />
+		</div>
+	)
+}
